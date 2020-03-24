949adb5d8fe583e1b22e6f0c6c41249d9fc99697
joppich <joppich@bricknet.de>
2020-03-24 02:21:43 +0100

    add License, enable json processing of client messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
+/*
+
+MIT License
+
+Copyright (c) 2020 joppich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+TODO: 
+ implement client message parsing
+ implement subscription table
+ implement subscribe to subscription
+ */
+
 extern crate actix;
 
 extern crate actix_web;
+
+extern crate serde;
 
 #[macro_use]
 extern crate log;
@@ -8,7 +40,6 @@ extern crate log;
 extern crate env_logger;
 
 use std::time::{Instant, Duration};
-use std::str;
 
 use actix::prelude::*;
 use actix_web::{
@@ -16,7 +47,8 @@ use actix_web::{
 };
 use actix_web_actors::ws;
 use actix_files as fs;
-
+use serde::{Deserialize,Serialize};
+use serde_json::Result as SerdeResult;
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
@@ -30,6 +62,14 @@ async fn wsa(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Err
 ///Run websocket via actor, track alivenes of clients
 struct WebSock {
     hb: Instant,
+}
+
+///SerDe schema for client messages
+#[derive(Serialize,Deserialize)]
+struct ClientMessageRaw {
+    #[serde(rename = "type")]
+    _type: String,
+    data: String
 }
 
 impl Actor for WebSock {
@@ -49,9 +89,10 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSock {
         ctx: &mut Self::Context,
     ) {
         match msg {
-            Ok(ws::Message::Binary(msg)) => {
+            Ok(ws::Message::Text(msg)) => {
                 self.hb = Instant::now();
-                info!("Client message: {}", str::from_utf8(&msg).unwrap())
+                let message: ClientMessageRaw = serde_json::from_str(&msg).unwrap();
+                info!("Message type: {}, Message data: {}", &message._type, &message.data);
             }
             Ok(ws::Message::Ping(msg)) => {
                 self.hb = Instant::now();
