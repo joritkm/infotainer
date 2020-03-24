2a5ae13268f8414fe61f4628d922defb824e7ab7
joppich <joppich@bricknet.de>
2020-03-24 02:20:50 +0100

    scaffold webclient

--- static/dist/infotainer.js
+++ static/dist/infotainer.js
@@ -1,19 +1,71 @@
+const SUB = "subscription";
+const MSG = "message";
+let MSGFIELD = null;
+let SUBFIELD = null;
 let socket = null;
+
 
 function connect() {
   disconnect();
   console.log("Connecting...");
   socket = new WebSocket("ws://localhost:8080/ws/");
-  socket.onopen = function() {
-    console.log("Connected.");
-
-  }
 }
 
 function disconnect() {
   if (socket != null) {
     console.log("Disconnecting...");
-    socket.close();
-    socket = null;
+    socket.onclose = function () {
+      console.log("Disconnected.")
+      socket = null;
+    };
   }
 }
+
+function new_sub_request(_data) {
+  if (!_data) {
+    _data = SUBFIELD.options[SUBFIELD.selectedIndex].value;
+  }
+  return {
+    type: SUB,
+    data: _data
+  }
+}
+
+function new_message(_data) {
+  if (!_data) {
+    _data = MSGFIELD.value;
+  }
+  MSGFIELD.value = "";
+  return {
+    type: MSG,
+    data: _data
+  }
+}
+
+function socket_send(_type, _data = null) {
+  let payload = null;
+  if (_type == "message") {
+    payload = new_message(_data);
+  } else if (_type == "subscription") {
+    payload = new_sub_request(_data);
+  } else {
+    throw _type + " is unknown"
+  }
+  if (!payload.data) {
+    throw "No data to send"
+  } 
+  if (socket == null) {
+    throw "Not connected to a socket"
+  }
+  console.log("Sending " + payload.type + " request: \n" + payload.data);
+  socket.send(JSON.stringify(payload));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  MSGFIELD = document.getElementById(MSG);
+  SUBFIELD = document.getElementById(SUB);
+  connect();
+  socket.onopen= function () {
+    socket_send(SUB, "datasets");
+  };
+});
