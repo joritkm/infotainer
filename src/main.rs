f140024cd464cf2e8d2433e663cf10251c7817f3
joppich <joppich@bricknet.de>
2020-04-15 17:11:12 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-TODO: 
+TODO:
  implement subscription table
  implement subscribe to subscription
  */
@@ -41,16 +41,14 @@ extern crate failure;
 
 extern crate env_logger;
 
-use std::time::{Instant, Duration};
 use std::convert::TryFrom;
 use std::fmt;
+use std::time::{Duration, Instant};
 
 use actix::prelude::*;
-use actix_web::{
-    middleware, web, http, App, Error, HttpRequest, HttpResponse, HttpServer, error
-};
-use actix_web_actors::ws;
 use actix_files as fs;
+use actix_web::{error, http, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
 
 use uuid::Uuid;
 
@@ -88,11 +86,7 @@ impl Actor for WebSock {
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSock {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Text(msg)) => {
                 self.hb = Instant::now();
@@ -115,7 +109,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSock {
 }
 
 impl WebSock {
-    
     fn new() -> Self {
         Self { hb: Instant::now() }
     }
@@ -134,21 +127,21 @@ impl WebSock {
 
 #[derive(Debug, Fail, PartialEq, Clone)]
 pub enum ParseError {
-    #[fail(display="Unknown request Parameter: {}", _0)]
+    #[fail(display = "Unknown request Parameter: {}", _0)]
     UnknownParameter(String),
 
-    #[fail(display="Unknown request Type: {}", _0)]
+    #[fail(display = "Unknown request Type: {}", _0)]
     UnknownType(String),
 
-    #[fail(display="Request parameter missing: {}", _0)]
-    MissingParameter(String)
+    #[fail(display = "Request parameter missing: {}", _0)]
+    MissingParameter(String),
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum ClientRequest {
-    Get {param: String },
+    Get { param: String },
     Add { param: String },
-    Remove { param: String }
+    Remove { param: String },
 }
 
 impl ClientRequest {
@@ -161,38 +154,34 @@ impl ClientRequest {
             .to_lowercase();
         let req_arg: String = params
             .next()
-            .ok_or(ParseError::MissingParameter(String::from("Request Argument")))
+            .ok_or(ParseError::MissingParameter(String::from(
+                "Request Argument",
+            )))
             .map_err(|e| error::ErrorBadRequest(e))?
             .chars()
-            .filter(|c| { c.is_alphanumeric() })
+            .filter(|c| c.is_alphanumeric())
             .take(256)
             .collect();
         match req_type.as_str() {
-            "get" => {
-                Ok(ClientRequest::Get{ param : req_arg })
-            },
-            "add" => {
-                Ok(ClientRequest::Add{ param: req_arg})
-            },
-            "remove" => {
-                Ok(ClientRequest::Remove{ param: req_arg})
-            },
-            _ => {
-                Err(ParseError::UnknownType(String::from(raw)))
-                    .map_err(|e| error::ErrorBadRequest(e))
-            }
-        }       
+            "get" => Ok(ClientRequest::Get { param: req_arg }),
+            "add" => Ok(ClientRequest::Add { param: req_arg }),
+            "remove" => Ok(ClientRequest::Remove { param: req_arg }),
+            _ => Err(ParseError::UnknownType(String::from(raw)))
+                .map_err(|e| error::ErrorBadRequest(e)),
+        }
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 struct ClientID {
-    uid: Uuid
+    uid: Uuid,
 }
 
 impl ClientID {
     fn new() -> ClientID {
-        ClientID {uid: Uuid::new_v4()}
+        ClientID {
+            uid: Uuid::new_v4(),
+        }
     }
 }
 
@@ -201,7 +190,7 @@ impl TryFrom<&str> for ClientID {
 
     fn try_from(id: &str) -> Result<ClientID, Self::Error> {
         let uid = Uuid::parse_str(&id)?;
-        Ok(ClientID { uid: uid})
+        Ok(ClientID { uid: uid })
     }
 }
 
@@ -226,16 +215,21 @@ impl ClientMessage {
         let msg_req = msg_token
             .next()
             .ok_or(error::ErrorBadRequest("Missing Request."))?;
-        let id = ClientID::try_from(msg_id)
-            .map_err(|e| error::ErrorForbidden(e))?;
+        let id = ClientID::try_from(msg_id).map_err(|e| error::ErrorForbidden(e))?;
         let request = ClientRequest::from_str(msg_req)?;
-        Ok(ClientMessage {id: id, req: request})   
+        Ok(ClientMessage {
+            id: id,
+            req: request,
+        })
     }
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info,infotainer=debug");
+    std::env::set_var(
+        "RUST_LOG",
+        "actix_server=info,actix_web=info,infotainer=debug",
+    );
     env_logger::init();
 
     HttpServer::new(|| {
