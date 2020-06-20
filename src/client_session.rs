c45f7072729058ab0792d31799fdc6b79d0f96f0
joppich <joppich@bricknet.de>
2020-06-20 02:08:12 +0200

    rename session -> client_session, Session->ClientSession, Sessions->ClientSessions

--- src/client_session.rs
+++ src/client_session.rs
@@ -5,53 +5,53 @@ use uuid::Uuid;
 
 use crate::client::ClientID;
 
-/// Holds a clients session state
+/// Holds a clients subscription state
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-pub struct Session {
+pub struct ClientSession {
     /// A `crate::client::ClientID`.
     client: ClientID,
-    /// Holds IDs of `crate::subscription::Subscription`.
+    /// Holds IDs of `crate::scubscription::Subscription`.
     subscriptions: Vec<uuid::Uuid>
 }
 
-impl From<(&Uuid,&Vec<Uuid>)> for Session {
-    /// Convert from entries in `crate::session::Sessions` 
-    fn from(entry: (&Uuid,&Vec<Uuid>)) -> Session {
-        Session {
+impl From<(&Uuid,&Vec<Uuid>)> for ClientSession {
+    /// Convert from entries in `crate::session::ClientSessions` 
+    fn from(entry: (&Uuid,&Vec<Uuid>)) -> ClientSession {
+        ClientSession {
             client: ClientID::from(entry.0.to_owned()),
             subscriptions: entry.1.to_owned()
         }
     }
 }
 
-/// Holds a `HashMap` of `Session` data
+/// Holds a `HashMap` of `ClientSession` data
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-pub struct Sessions {
+pub struct ClientSessions {
     store: HashMap<Uuid, Vec<Uuid>>,
 }
 
-impl Sessions {
-    /// Initializes a new Session store
-    pub fn new() -> Sessions {
-        Sessions {
+impl ClientSessions {
+    /// Initializes a new ClientSession store
+    pub fn new() -> ClientSessions {
+        ClientSessions {
             store: HashMap::new(),
         }
     }
 
-    /// Tries to retrieve an entry from the Session store. If it doesn't
+    /// Tries to retrieve an entry from the ClientSession store. If it doesn't
     /// exist, it will create a new entry from the provided `crate::client::ClientID`.
-    pub fn get_or_insert(&mut self, cli: &ClientID) -> Session {
+    pub fn get_or_insert(&mut self, cli: &ClientID) -> ClientSession {
         if let Some(entry) = self.store.get_key_value(&cli.id()) {
-            Session::from(entry)
+            ClientSession::from(entry)
         } else {
             self.store.insert(cli.id().to_owned(), Vec::new());
             self.get_or_insert(cli)
         }
     }
 
-    /// Updates a specific entry in the Session store
+    /// Updates a specific entry in the ClientSession store
     // TODO: make this fail if entry doesn't exist.
-    pub fn update(&mut self, session: &Session) {
+    pub fn update(&mut self, session: &ClientSession) {
         self.store.insert(session.client.id().to_owned(), session.subscriptions.to_owned());
     }
 
@@ -67,19 +67,19 @@ pub mod tests {
 
     #[test]
     fn test_session() {
-        let dummy = Session{ client: ClientID::from(Uuid::new_v4()),
+        let dummy = ClientSession{ client: ClientID::from(Uuid::new_v4()),
                              subscriptions: vec![Uuid::new_v4(),Uuid::new_v4()] };
         let session_entry = (&dummy.client.id(), &dummy.subscriptions);
-        let session: Session = session_entry.into();
+        let session: ClientSession = session_entry.into();
         assert_eq!(session, dummy);
     }
 
     #[test]
     fn test_sessions() {
         let subscription = Uuid::new_v4();
-        let mut sessions = Sessions::new();
+        let mut sessions = ClientSessions::new();
         let mut session = sessions.get_or_insert(&ClientID::from(Uuid::new_v4()));
-        let session_entry = Session{ client: session.client.to_owned(), subscriptions: vec![]};
+        let session_entry = ClientSession{ client: session.client.to_owned(), subscriptions: vec![]};
         assert_eq!(session, session_entry);
         session.subscriptions.push(subscription);
         sessions.update(&session);
--- src/main.rs
+++ src/main.rs
@@ -40,13 +40,13 @@ use actix_files as fs;
 use tokio::sync::Mutex;
 
 use websocket::WebSock;
-use session::Sessions;
+use client_session::ClientSessions;
 use subscription::Subscriptions;
 use client::ClientID;
 
 mod errors;
 mod websocket;
-mod session;
+mod client_session;
 mod subscription;
 mod client;
 
@@ -60,10 +60,10 @@ async fn wsa(req: web::HttpRequest, stream: web::Payload) -> Result<web::HttpRes
 }
 
 ///Create a new ClientID
-async fn client_id(sessions: web::Data<Mutex<Sessions>>) -> Result<web::HttpResponse, error::Error> {
+async fn client_id(sess_client: web::Data<Mutex<ClientSessions>>) -> Result<web::HttpResponse, error::Error> {
     // TODO: Pending the renaming of sessions, remove this and accept posts with their own uuids from new clients. 
     let client_id = ClientID::try_from("89e93972-cb91-4511-944d-30de98a87199").unwrap();
-    let client = sessions.try_lock().map_err(|e| error::ErrorInternalServerError(e))?.get_or_insert(&client_id);
+    let client = sess_client.try_lock().map_err(|e| error::ErrorInternalServerError(e))?.get_or_insert(&client_id);
     Ok(web::HttpResponse::build(http::StatusCode::OK)
         .content_type("application/json; charset=utf-8")
         .json(client))
@@ -76,7 +76,7 @@ async fn main() -> std::io::Result<()> {
         "actix_server=info,actix_web=info,infotainer=debug",
     );
     env_logger::init();
-    let sessions = web::Data::new(Mutex::new(Sessions::new()));
+    let sessions = web::Data::new(Mutex::new(ClientSessions::new()));
     let subscriptions = web::Data::new(Mutex::new(Subscriptions::new()));
 
     HttpServer::new(move || {
