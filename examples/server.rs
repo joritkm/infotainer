e2bc503dd68f946da44d7aaaf802a2939b5e5ca9
joppich <joppich@bricknet.de>
2020-12-26 19:00:49 +0100

    update server example

--- examples/server.rs
+++ examples/server.rs
@@ -1,18 +1,22 @@
-use std::fs::create_dir;
-use std::path::{PathBuf};
+use std::fs::create_dir_all;
+use std::path::PathBuf;
 
 use actix::prelude::Actor;
 use actix_web::{middleware, web, App, HttpServer};
 
-use infotainer::prelude::{ DataLogger, PubSubServer, websocket_handler};
+use infotainer::prelude::{websocket_handler, DataLogger, PubSubServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     let data_path = PathBuf::from("/tmp/infotainer-server-example");
-    create_dir(&data_path)?;
-    let data_logger_addr = DataLogger::new(&data_path).expect("Could not initiate DataLogger").start();
-    let pubsub_server_addr = PubSubServer::new(Some(&data_logger_addr)).expect("Could not initiate PubSubServer.").start();
+    create_dir_all(&data_path)?;
+    let data_logger_addr = DataLogger::new(&data_path)
+        .expect("Could not initiate DataLogger")
+        .start();
+    let pubsub_server_addr = PubSubServer::new(Some(&data_logger_addr))
+        .expect("Could not initiate PubSubServer.")
+        .start();
     HttpServer::new(move || {
         App::new()
             .data(pubsub_server_addr.clone())
